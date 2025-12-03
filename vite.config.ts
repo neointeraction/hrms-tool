28cf6f2b13783d7bb09b9407f6bcc167e96978bf
mshameer237 <mshameer237@gmail.com>
2025-12-03 13:09:35 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// **!!! CRITICAL CHANGE HERE !!!**
-// 1. Replace 'your-repo-name' with the exact name of your GitHub repository.
-const repoName = "neointeraction";
-
-// https://vitejs.dev/config/
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-
-  // Set the base path for deployment to GitHub Pages (e.g., /my-awesome-app/)
-  base: `/${repoName}/`,
 });
